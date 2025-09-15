14ff00376cba7e821c75da0b56db76af06a39ac6
Adity Bhattacharyya <aditybhattacharyya2005@gmail.com>
2025-09-15 11:32:30 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,12 +1,13 @@
-# Function to find largest element
+
 def find_largest(arr):
-    largest = arr[0]  # assume first element is largest
+    largest = arr[0] 
     for num in arr:
         if num > largest:
             largest = num
     return largest
 
 # Examples
-print(find_largest([1, 8, 7, 56, 90]))  # Output: 90
-print(find_largest([5, 5, 5, 5]))       # Output: 5
-print(find_largest([10]))               # Output: 10
+print(find_largest([1, 8, 7, 56, 90]))  
+print(find_largest([5, 5, 5, 5]))      
+print(find_largest([10]))             
+
