b9ed28afa29806947a9324e44f3794b9c597033f
Adity Bhattacharyya <aditybhattacharyya2005@gmail.com>
2025-09-15 11:31:48 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,13 +1,14 @@
-# Function to insert at the end
+
 def insert_at_end(arr, val):
-    arr.append(val)  # append adds element at the end
+    arr.append(val) 
     return arr
 
-# Example usage
+
 arr1 = [1, 2, 3, 4, 5]
 val1 = 90
-print(insert_at_end(arr1, val1))  # Output: [1, 2, 3, 4, 5, 90]
+print(insert_at_end(arr1, val1))  
 
 arr2 = [1, 2, 3]
 val2 = 50
-print(insert_at_end(arr2, val2))  # Output: [1, 2, 3, 50]
+print(insert_at_end(arr2, val2)) 
+
